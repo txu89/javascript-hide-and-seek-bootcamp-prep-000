661693439f38692879f542cd60fa96d6dd17df8a
Tom Xu <tomxu89@gmail.com>
2018-01-15 05:19:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById(nested).querySelector('.target')
+  return document.getElementById('nested').querySelector('.target')
 }
 
+function increaseRankBy(n) {
+  
+}
